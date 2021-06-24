3226e630c54c64ef709bd840c33126da32675851
jelenamarjanovic <jelena.marjanovic.13@gmail.com>
2021-06-25 00:09:17 +0200

    Connect all together

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {getUsersRequest} from '../actions/users';
 
 class App extends Component {
   render() {
@@ -10,4 +12,6 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(({users}) => ({users}), {
+  getUsersRequest
+})(App);
